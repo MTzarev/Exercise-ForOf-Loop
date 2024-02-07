c822e23547030725a7b9072e62ac282eee26407f
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-07 13:36:17 +0200

    Add files via upload

---
+++ Exercise forOfLoop.js
@@ -0,0 +1,14 @@
+function forOfLoop (array1, array2){
+    let final ="Welcome";
+for (str of array1) {
+    if (array2.includes(str)){
+        console.log(final);
+    }else {
+   
+    }
+     
+    
+}
+    console.log();
+}
+forOfLoop(['Hey', 'hello', 2, 4, 'Peter', 'e'],['Petar', 10, 'hey', 4, 'hello', '2']);
